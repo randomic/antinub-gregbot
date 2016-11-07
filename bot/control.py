2370bc7ad4cead39fad8195d7719a8fbed6d0cd1
randomic <j.a.d.mcmillan@gmail.com>
2016-11-07 12:08:33 +0000

    Reworked to use Bot subclass of Discord client

---
+++ bot/control.py
@@ -0,0 +1,32 @@
+import logging
+import config
+
+import discord.ext.commands as commands
+
+
+def setup(client):
+    client.add_cog(Control(client))
+
+
+# Helper functions
+def isOwner(context):
+    'Check whether or not the user is the owner of the bot'
+    return context.message.author.id == config.OWNER_ID
+
+
+class Control:
+    '''A cog defining commands for controlling the
+    bot's operation such as stopping the bot'''
+    def __init__(self, client):
+        self.logger = logging.getLogger(__name__)
+        self.client = client
+
+    async def on_command_error(self, exception, context):
+            self.logger.debug(type(exception))
+    
+    @commands.command()
+    @commands.check(isOwner)
+    async def stop(self):
+        '''Logs the bot out of discord and stops it'''
+        await self.client.say('до свидания')
+        await self.client.logout()
