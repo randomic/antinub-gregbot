3e50e9c2e9324960c11eee9e88b318cf5155db1f
randomic <j.a.d.mcmillan@gmail.com>
2016-11-21 13:33:58 +0000

    Refactored the config for extensions

--- ext/killmails.py
+++ ext/killmails.py
@@ -4,11 +4,13 @@ Killmail posting cog for antinub-gregbot project.
 Monitors zKillboard's redisQ api and posts killmails relevant to your corp in
 the given channel.
 '''
-from asyncio import sleep as async_sleep
+from asyncio import sleep
 import logging
 
-import aiohttp
+from aiohttp import ClientError
 from discord.compat import create_task
+
+from config import KILLMAILS
 
 
 def setup(bot):
@@ -22,39 +24,50 @@ class Killmails:
     def __init__(self, bot):
         self.logger = logging.getLogger(__name__)
         self.bot = bot
-        self.channel = self.bot.get_channel('244849123759489024')
-        # self.corp_id = 98199571
-        self.corp_id = 98388312
+        self.channel = self.bot.get_channel(KILLMAILS['channel_id'])
+        self.corp_id = KILLMAILS['corp_id']
+        # self.corp_id = 98388312
+        self.url = KILLMAILS['redisQ_uri']
+        if 'is_relevant' in KILLMAILS:
+            self.is_relevant = KILLMAILS['is_relevant']
+        else:
+            self.is_relevant = self._default_is_relevant
         self.listening = False
-        self.url = 'http://redisq.zkillboard.com/listen.php'
+
         self.start_listening()
 
     def get_status(self):
         'Returns a string describing the status of this cog'
-        return 'Listening' if self.listening else 'Not listening'
+        if self.listening:
+            return '\n  \u2714 Listening'
+        else:
+            return '\n  \u2716 Not listening'
 
-    def is_relevant(self, package):
+    def _default_is_relevant(self, package):
         'Returns true if the killmail should be relayed to discord'
-        if package['killmail']['victim']['corporation']['id'] == self.corp_id:
-            return True
+        victim_corp = package['killmail']['victim']['corporation']
+        if victim_corp['id'] == self.corp_id:
+            return '{} loss'.format(victim_corp['name'])
         for attacker in package['killmail']['attackers']:
             if 'corporation' in attacker:
                 if attacker['corporation']['id'] == self.corp_id:
-                    return True
-        return False
+                    return '{} kill'.format(attacker['corporation']['name'])
+        return ''
 
     async def handle_package(self, package):
         'Checks is_relevant to see if the killmail needs posting to discord'
         if package:
-            if self.is_relevant(package):
+            relevancy_string = self.is_relevant(package)
+            if relevancy_string:
                 kill_id = package['killID']
-                message = 'https://zkillboard.com/kill/{}/'.format(kill_id)
+                message = '**{}**\n  '.format(relevancy_string)
+                message += 'https://zkillboard.com/kill/{}/'.format(kill_id)
                 await self.bot.send_message(self.channel, message)
                 self.logger.info('Posted a killmail')
             else:
-                self.logger.info('Ignored a killmail')
+                self.logger.debug('Ignored a killmail')
         else:
-            self.logger.info('No kills retrieved')
+            self.logger.debug('No kills retrieved')
 
     async def retrieve_kills(self):
         'Returns a dictionary containing the contents of the redisQ package'
@@ -63,28 +76,23 @@ class Killmails:
             zkb_data = await response.json()
             return zkb_data['package']
 
-    async def listen_loop(self):
+    async def _run_listen_loop(self):
         '''Loops to try to retrieve killmail packages'''
         while self.listening:
-            await async_sleep(3)
             try:
                 package = await self.retrieve_kills()
                 await self.handle_package(package)
-            except Exception as exc:
+                await sleep(0.25)
+            except AssertionError as exc:
                 self.logger.exception(exc)
+            except ClientError as exc:
+                self.logger.exception(exc)
+        self.logger.info('Loop exited')
 
     def start_listening(self):
-        'Starts the task of checking for new killmails every 5 minutes'
+        'Starts the task of checking for new killmails'
         self.listening = True
-        create_task(self.listen_loop(), loop=self.bot.loop)
+        create_task(self._run_listen_loop())
 
     def __unload(self):
         self.listening = False
-
-# if __name__ == '__main__':
-#     logging.basicConfig()
-#     logging.getLogger().setLevel(logging.INFO)
-#     with open('../test.json', 'rt') as data_json:
-#         data_dict = json.load(data_json)
-#     logger = logging.getLogger(__name__)
-#     logger.info(data_dict['package']['zkb']['points'])
