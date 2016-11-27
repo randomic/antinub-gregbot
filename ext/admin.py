439f880f3662639eb1cbb126989821e8e05e27aa
steveizi <ninjagames01@hotmail.co.uk>
2016-11-27 01:27:12 +0000

    Added demotion and promotion to admin.py
    - Seperated getting member objects via ID or name to a function called
    "get_memberobj"
    - Finished the "promote" command, it now works fully.
    - Wrote the "demote" command

--- ext/admin.py
+++ ext/admin.py
@@ -1,3 +1,8 @@
+'''
+Admin cog made by Steveizi for antinub-gregbot project.
+
+Contains several commands regarding admin management
+'''
 import logging
 from util import is_owner
 
@@ -5,6 +10,7 @@ import discord.ext.commands as commands
 
 
 def setup(bot):
+    'Adds the cog to the provided discord bot'
     bot.add_cog(Admin(bot))
 
 
@@ -25,28 +31,65 @@ class Admin:
         self.logger = logging.getLogger(__name__)
         self.bot = bot
 
+    def get_status(self):
+        'Returns a string describing the status of this cog'
+        return '\n  \u2714 Cog loaded, ready to use'
+
+    def get_memberobj(self, context, user):
+        '''A function which returns either a member object of the given
+        ID/User or None if it cannot get it'''
+        if context.message.server is not None:
+            server = context.message.server
+            self.logger.info('Kappa1')
+            if user.isdecimal():
+                self.logger.info('User entered valid ID: %s', user)
+                return server.get_member(user)
+            else:
+                self.logger.info('User entered valid name: %s', user)
+                return server.get_member_named(user)
+        else:
+            return "PM"
+
     @commands.command(pass_context=True)
     @commands.check(is_owner)
     async def promote(self, ctx, promotee: str):
         '''Command that promotes a user via ID or name to a role called
         "admin", given that it has the permissions and the role exists.
         ID is most accurate, then Name#0000, then Name.'''
-        if ctx.message.server is not None:
-            server = ctx.message.server
-            self.logger.info('Kappa1')
-            if promotee.isdecimal():
-                member = server.get_member(promotee)
-                self.logger.info('User entered valid ID: %s', promotee)
-            else:
-                member = server.get_member_named(promotee)
-                self.logger.info('User entered valid name: %s', promotee)
-            if member is not None:
-                await self.bot.say('Invalid member name/ID entered.')
-                self.logger.warning('User entered invalid Name or ID')
-            else:
-                roles = ['admin']
-                await server.add_roles(member, roles)
-                await self.bot.say('%s promoted to admin!' % member.name)
-        else:
+        member = self.get_memberobj(ctx, promotee)
+        if member is None:
+            await self.bot.say('Invalid member name/ID entered.')
+            self.logger.warning('User entered invalid Name or ID')
+        elif member == "PM":
             await self.bot.say('You can\'t promote someone in a private chat.')
             self.logger.warning('User tried to promote in PM')
+        else:
+            roleobj = None
+            server = ctx.message.server
+            for role in server.roles:
+                if role.name == 'admin':
+                    roleobj = role
+            await self.bot.add_roles(member, roleobj)
+            await self.bot.say('%s promoted to admin!' % promotee)
+
+    @commands.command(pass_context=True)
+    @commands.check(is_owner)
+    async def demote(self, ctx, demotee: str):
+        '''Command that demotes a user via ID or name from a role called
+        "admin", given that it has the permissions and the role exists.
+        ID is most accurate, then Name#0000, then Name.'''
+        member = self.get_memberobj(ctx, demotee)
+        if member is None:
+            await self.bot.say('Invalid member name/ID entered.')
+            self.logger.warning('User entered invalid Name or ID')
+        elif member == "PM":
+            await self.bot.say('You can\'t demote someone in a private chat.')
+            self.logger.warning('User tried to demote in PM')
+        else:
+            roleobj = None
+            server = ctx.message.server
+            for role in server.roles:
+                if role.name == 'admin':
+                    roleobj = role
+            await self.bot.remove_roles(member, roleobj)
+            await self.bot.say('%s demoted from admin!' % demotee)
