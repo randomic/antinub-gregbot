fc75be36a446b08f65388fde4a531bc3bc092dd1
steveizi <ninjagames01@hotmail.co.uk>
2016-11-29 17:57:39 +0000

    Renamed admin, moved to groupcheck.py
    Added all groupchecks which read specified groups from the config.
    Includes:
    - is_owner
    - is_admin
    - is_director
    - is_respected
    - is_member

--- ext/groupcheck.py
+++ ext/groupcheck.py
@@ -4,7 +4,7 @@ Admin cog made by Steveizi for antinub-gregbot project.
 Contains several commands regarding admin management
 '''
 import logging
-from util import is_owner
+import config
 
 import discord.ext.commands as commands
 
@@ -14,21 +14,49 @@ def setup(bot):
     bot.add_cog(Admin(bot))
 
 
+def is_owner(context):
+    'Check whether or not the user is the owner of the bot'
+    return calculate(context, 5)
+
+
 def is_admin(context):
     'Check whether or not the user is an admin of the server'
+    return calculate(context, 4)
+
+
+def is_director(context):
+    'Check whether or not the user is a director of the server'
+    return calculate(context, 3)
+
+
+def is_respected(context):
+    'Check whether or not the user is a respected user of the server'
+    return calculate(context, 2)
+
+
+def is_member(context):
+    'Check whether or not the user is a member of the server'
+    return calculate(context, 1)
+
+
+def calculate(context, level: int):
+    '''Checks whether the user has a high enough "level" of permissions by
+    comparing a pre-defined list of group permissions with the level of
+    permission given as an argument'''
     role_list = context.message.author.roles
-    admin_role = "admin"
-    if is_owner(context):
+    server_id = context.message.server.id
+    if context.message.author.id == config.OWNER_ID:
         return True
     else:
         for role in role_list:
-            if role.name == admin_role:
-                return True
+            if role.name in config.UTIL['groups']:
+                if config.UTIL['groups'][server_id][role.name] > level:
+                    return True
 
 
-class Admin:
-    '''A cog defining commands for controlling the
-    bot's operation such as stopping the bot'''
+class GroupManagement:
+    '''A cog defining commands regarding group management
+    and group checks'''
     def __init__(self, bot):
         self.logger = logging.getLogger(__name__)
         self.bot = bot
@@ -51,7 +79,7 @@ class Admin:
 
     @commands.command(pass_context=True)
     @commands.check(is_owner)
-    async def promote(self, ctx, promotee: str):
+    async def promote(self, ctx, promotee: str, role: str):
         '''Command that promotes a user via ID or name to a role called
         "admin", given that it has the permissions and the role exists.
         ID is most accurate, then Name#0000, then Name.'''
@@ -65,15 +93,18 @@ class Admin:
         else:
             roleobj = None
             server = ctx.message.server
-            for role in server.roles:
-                if role.name == 'admin':
-                    roleobj = role
+            for serverrole in server.roles:
+                if role.name == role:
+                    roleobj = serverrole
+            if roleobj is None:
+                await self.bot.say('Role not found on the server.')
+                self.logger.warning('User entered an invalid role.')
             await self.bot.add_roles(member, roleobj)
-            await self.bot.say('%s promoted to admin!' % promotee)
+            await self.bot.say('%s promoted to %s!' % promotee, role)
 
     @commands.command(pass_context=True)
     @commands.check(is_owner)
-    async def demote(self, ctx, demotee: str):
+    async def demote(self, ctx, demotee: str, role: str):
         '''Command that demotes a user via ID or name from a role called
         "admin", given that it has the permissions and the role exists.
         ID is most accurate, then Name#0000, then Name.'''
@@ -87,11 +118,14 @@ class Admin:
         else:
             roleobj = None
             server = ctx.message.server
-            for role in server.roles:
-                if role.name == 'admin':
-                    roleobj = role
+            for serverrole in server.roles:
+                if role.name == role:
+                    roleobj = serverrole
+            if roleobj is None:
+                await self.bot.say('Role not found on the server.')
+                self.logger.warning('User entered an invalid role.')
             await self.bot.remove_roles(member, roleobj)
-            await self.bot.say('%s demoted from admin!' % demotee)
+            await self.bot.say('%s demoted from %s!' % demotee, role)
 
     @commands.command()
     @commands.check(is_admin)
