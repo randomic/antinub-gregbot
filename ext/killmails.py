f59e8a261692389b36b0b8759849e1c745a87834
randomic <j.a.d.mcmillan@gmail.com>
2016-12-05 17:24:14 +0000

    Switched killmails extension to use embeds

--- ext/killmails.py
+++ ext/killmails.py
@@ -4,11 +4,13 @@ Killmail posting cog for antinub-gregbot project.
 Monitors zKillboard's redisQ api and posts killmails relevant to your corp in
 the given channel.
 '''
-from asyncio import sleep
+import asyncio
+from datetime import datetime
 import logging
 
 from aiohttp import ClientError
 from discord.compat import create_task
+from discord.embeds import Embed
 
 from config import KILLMAILS
 
@@ -25,14 +27,12 @@ class Killmails:
         self.logger = logging.getLogger(__name__)
         self.bot = bot
         self.channel = self.bot.get_channel(KILLMAILS['channel_id'])
-        self.corp_id = KILLMAILS['corp_id']
         # self.corp_id = 98388312
-        self.url = KILLMAILS['redisQ_uri']
-        if 'is_relevant' in KILLMAILS:
-            self.is_relevant = KILLMAILS['is_relevant']
-        else:
-            self.is_relevant = self._default_is_relevant
         self.listening = False
+        self.url = KILLMAILS['redisQ_url']
+
+        self.corp_ids = KILLMAILS['corp_ids']
+        self.others_value = KILLMAILS['others_value']
 
         self.start_listening()
 
@@ -43,26 +43,66 @@ class Killmails:
         else:
             return '\n  \u2716 Not listening'
 
-    def _default_is_relevant(self, package):
-        'Returns true if the killmail should be relayed to discord'
-        victim_corp = package['killmail']['victim']['corporation']
-        if victim_corp['id'] == self.corp_id:
-            return '{} loss'.format(victim_corp['name'])
+    def is_relevant(self, package):
+        'Returns True if a killmail should be relayed to discord'
+        value = package['zkb']['totalValue'] / 1000000.0
+        if value >= self.others_value and self.others_value:
+            return True
+
+        victim_corp_id = package['killmail']['victim']['corporation']['id_str']
+        if victim_corp_id in self.corp_ids:
+            if value >= self.corp_ids[victim_corp_id]:
+                return True
+
         for attacker in package['killmail']['attackers']:
             if 'corporation' in attacker:
-                if attacker['corporation']['id'] == self.corp_id:
-                    return '{} kill'.format(attacker['corporation']['name'])
-        return ''
+                attacker_corp_id = attacker['corporation']['id_str']
+                if attacker_corp_id in self.corp_ids:
+                    if value >= self.corp_ids[attacker_corp_id]:
+                        return True
+
+        return False
+
+    def killmail_embed(self, package):
+        'Generates the embed which the killmail will be posted in'
+        victim = package['killmail']['victim']
+        victim_name = victim['character']['name']
+        ship_name = victim['shipType']['name']
+        ship_id = victim['shipType']['id_str']
+        bare_url = 'http://imageserver.eveonline.com/Type/{}_64.png'
+        ship_icon_url = bare_url.format(ship_id)
+
+        killtitle = '{} | {} | Killmail'.format(ship_name, victim_name)
+
+        bareinfo = '{} ({}) lost their {} in {}\n\nTotal Value: {:,} ISK'
+        killinfo = bareinfo.format(victim_name,
+                                   victim['corporation']['name'],
+                                   ship_name,
+                                   package['killmail']['solarSystem']['name'],
+                                   package['zkb']['totalValue'])
+
+        killurl = 'https://zkillboard.com/kill/{}/'.format(package['killID'])
+        killtime = package['killmail']['killTime']
+        if victim['corporation']['id_str'] in self.corp_ids:
+            killcolour = 0x7a0000  # red
+        else:
+            killcolour = 0x007a00  # green
+
+        embed = Embed(
+            title=killtitle,
+            description=killinfo,
+            url=killurl,
+            timestamp=datetime.strptime(killtime, '%Y.%m.%d %H:%M:%S'),
+            colour=killcolour)
+        embed.set_thumbnail(url=ship_icon_url)
+        return embed
 
     async def handle_package(self, package):
         'Checks is_relevant to see if the killmail needs posting to discord'
         if package:
-            relevancy_string = self.is_relevant(package)
-            if relevancy_string:
-                kill_id = package['killID']
-                message = '**{}**\n  '.format(relevancy_string)
-                message += 'https://zkillboard.com/kill/{}/'.format(kill_id)
-                await self.bot.send_message(self.channel, message)
+            if self.is_relevant(package):
+                embed = self.killmail_embed(package)
+                await self.bot.send_message(self.channel, embed=embed)
                 self.logger.info('Posted a killmail')
             else:
                 self.logger.debug('Ignored a killmail')
@@ -82,12 +122,12 @@ class Killmails:
             try:
                 package = await self.retrieve_kills()
                 await self.handle_package(package)
-                await sleep(0.25)
+                await asyncio.sleep(0.25)
             except AssertionError as exc:
                 self.logger.exception(exc)
             except ClientError as exc:
                 self.logger.exception(exc)
-        self.logger.info('Loop exited')
+        self.logger.info('Loop done')
 
     def start_listening(self):
         'Starts the task of checking for new killmails'
