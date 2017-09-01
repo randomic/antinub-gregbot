9549d6c9feb627bf7acf0b456ea3383fc6817886
randomic <j.a.d.mcmillan@gmail.com>
2017-09-01 14:27:35 +0100

    Organise util and core modules

--- core/control.py
+++ core/control.py
@@ -20,33 +20,6 @@ def setup(bot):
     bot.add_cog(Control(bot))
 
 
-def paginate(string, pref='```\n', aff='```', max_length=2000, sep='\n'):
-    'Chops a string into even chunks of max_length around the given separator'
-    max_size = max_length - len(pref) - len(aff)
-
-    str_length = len(string)
-    if str_length <= max_size:
-        return [pref + string + aff]
-    else:
-        split = string.rfind(sep, 0, max_size) + 1
-        if split:
-            return ([pref + string[:split] + aff]
-                    + paginate(string[split:], pref, aff, max_length, sep))
-        else:
-            return ([pref + string[:max_size] + aff]
-                    + paginate(string[max_size:], pref, aff, max_length, sep))
-
-
-async def notify_admins(bot, messages):
-    'Send message to the private channel of each admin'
-    recipients = set(config.ADMINS)
-    recipients.add(config.OWNER_ID)  # Include owner if not already there
-    for user_id in recipients:
-        channel = await bot.get_user_info(user_id)
-        for message in messages:
-            await bot.send_message(channel, message)
-
-
 class Control:
     '''A cog defining commands for controlling the
     bot's operation such as stopping the bot'''
