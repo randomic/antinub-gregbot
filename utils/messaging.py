01b510cf58c5207c711461d83225a640c00712b5
randomic <j.a.d.mcmillan@gmail.com>
2017-09-01 14:27:35 +0100

    Organise util and core modules

---
+++ utils/messaging.py
@@ -0,0 +1,32 @@
+"""
+Contains utilities regarding messages
+"""
+
+
+def paginate(string, pref='```\n', aff='```', max_length=2000, sep='\n'):
+    'Chop a string into even chunks of max_length around the given separator'
+    max_size = max_length - len(pref) - len(aff)
+
+    str_length = len(string)
+    if str_length <= max_size:
+        return [pref + string + aff]
+    else:
+        split = string.rfind(sep, 0, max_size) + 1
+        if split:
+            return ([pref + string[:split] + aff]
+                    + paginate(string[split:], pref, aff, max_length, sep))
+        else:
+            return ([pref + string[:max_size] + aff]
+                    + paginate(string[max_size:], pref, aff, max_length, sep))
+
+
+async def notify_owner(bot, messages):
+    'Send message to the private channel of each admin'
+    recipients = set(config.ADMINS)
+    recipients.add(config.OWNER_ID)  # Include owner if not already there
+    for user_id in recipients:
+        channel = await bot.get_user_info(user_id)
+        for message in messages:
+            await bot.send_message(channel, message)
+
+
