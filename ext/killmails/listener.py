f6789749b3cf02f61520b37fce175ab9c1faafa0
randomic <j.a.d.mcmillan@gmail.com>
2018-05-13 13:43:02 +0100

    Add skeleton of looping task

--- ext/killmails/listener.py
+++ ext/killmails/listener.py
@@ -3,6 +3,7 @@ Killmail fetching cog for antinub-gregbot project.
 
 Polls zKillboard's redisQ API and dispatches relevant killmails as an event.
 '''
+import asyncio
 import logging
 
 import discord.ext.commands as commands
@@ -15,11 +16,38 @@ def setup(bot: commands.Bot):
 class RedisQListener:
     '''Polls zKillboard's redisQ API and dispatches events containing killmails
     which pass the relevancy test'''
-    redisq_polling_task = None
 
     def __init__(self, bot: commands.Bot):
         self.logger = logging.getLogger(__name__)
-        self.logger.info("%s %s", type(self).__name__, 3)
+        self.logger.info('%s %s', type(self).__name__, 3)
+        self.bot = bot
+        self.redisq_polling_task = self.listen_task_start()
 
     def __unload(self):
-        self.logger.info("unloaded")
+        self.redisq_polling_task.cancel()
+
+    def listen_task_start(self, delay: int = 0) -> asyncio.Task:
+        task = self.bot.loop.create_task(self.wait_for_package(delay))
+        self.redisq_polling_task.add_done_callback(self.listen_task_done)
+        return task
+
+    def listen_task_done(self, task: asyncio.Task):
+        delay = 0
+        try:
+            package = task.result()
+            self.process_result(package)
+        except asyncio.CancelledError:
+            return
+        except Exception:
+            message = 'Error in killmail retrieve task'
+            self.logger.exception(message)
+            delay = 10
+
+        self.listen_task_start(delay)
+
+    async def wait_for_package(self, delay: int):
+        await asyncio.sleep(delay)
+        raise NotImplementedError
+
+    def process_result(self, package: dict):
+        raise NotImplementedError
