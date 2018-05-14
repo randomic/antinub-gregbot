75623ee619f7fd4b4369cc37b30dcd6f8d0b5071
randomic <j.a.d.mcmillan@gmail.com>
2018-05-15 00:08:06 +0100

    Implement basic structure of killmail poster

--- ext/killmails/poster.py
+++ ext/killmails/poster.py
@@ -1,6 +1,10 @@
 import logging
 
+import esipy
 from discord.ext import commands
+
+
+ESI_SWAGGER_JSON = 'https://esi.evetech.net/latest/swagger.json'
 
 
 def setup(bot: commands.Bot):
@@ -8,9 +12,19 @@ def setup(bot: commands.Bot):
 
 
 class KillmailPoster:
-    def __init__(self, bot):
+    def __init__(self, bot: commands.Bot):
         self.logger = logging.getLogger(__name__)
-        self.logger.info("%s %s", type(self).__name__, 3)
+        self.bot = bot
+        self.relevancy_table = self.bot.tdb.table("killmails.relevancies")
 
-    def __unload(self):
-        self.logger.info("unloaded")
+        self.esi_app = esipy.App.create(url=ESI_SWAGGER_JSON)
+        self.esi_client = esipy.EsiClient()
+
+    async def on_killmail(self, killmail: dict):
+        if not self.is_relevant(killmail):
+            return
+
+
+    def is_relevant(self, killmail: dict) -> bool:
+        return True
+
