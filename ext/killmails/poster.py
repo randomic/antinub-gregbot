19a776b5d5b99f45a7036d9e9b2e4fe660c0ae1b
randomic <j.a.d.mcmillan@gmail.com>
2018-05-15 14:42:21 +0100

    Change poster to be subclass of EsiCog

--- ext/killmails/poster.py
+++ ext/killmails/poster.py
@@ -1,8 +1,8 @@
 import logging
 
-import esipy
 from discord.ext import commands
 
+from utils.esicog import EsiCog
 
 ESI_SWAGGER_JSON = 'https://esi.evetech.net/latest/swagger.json'
 
@@ -11,20 +11,19 @@ def setup(bot: commands.Bot):
     bot.add_cog(KillmailPoster(bot))
 
 
-class KillmailPoster:
+class KillmailPoster(EsiCog):
     def __init__(self, bot: commands.Bot):
+        super(KillmailPoster, self).__init__(bot)
+
         self.logger = logging.getLogger(__name__)
         self.bot = bot
         self.relevancy_table = self.bot.tdb.table("killmails.relevancies")
 
-        self.esi_app = esipy.App.create(url=ESI_SWAGGER_JSON)
-        self.esi_client = esipy.EsiClient()
-
     async def on_killmail(self, killmail: dict):
         if not self.is_relevant(killmail):
             return
-
+        esi_app = await self.get_esi_app(self.bot)
+        self.logger.info(esi_app)
 
     def is_relevant(self, killmail: dict) -> bool:
         return True
-
